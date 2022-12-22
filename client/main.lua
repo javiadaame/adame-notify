317ffec6b4d75106062da7b64d3cd2c5e88d3187
Javi Adame <35896509+NickBlade@users.noreply.github.com>
2022-12-22 15:22:40 +0100

    Upload files

---
+++ client/main.lua
@@ -0,0 +1,8 @@
+JA = {}
+
+exports('GetObject', function()
+    return JA
+end)
+
+-- To use export in a script:
+-- local JA = exports['ja-base']:GetObject()
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+version '1.0.0'
+
+description ''
+
+lua54 'yes'
+
+use_fxv2_oal 'yes'
+
+ui_page 'web/index.html'
+
+client_scripts {'client/main.lua'}
+files {'web/*.*'}
